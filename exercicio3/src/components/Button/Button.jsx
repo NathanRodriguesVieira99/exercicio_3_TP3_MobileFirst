109f580f1a41a3ca57dc70ce7bd7f549421776b0
NathanRodrigues59 <nathanrodriguesvieira99@gmail.com>
2024-09-13 10:43:03 -0300

    criei o button

--- exercicio3/src/components/Button/Button.jsx
+++ exercicio3/src/components/Button/Button.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { useState } from 'react'
+
 
 function Button() {
+
+  const [like, setLike] = useState(0)
+  const [dislike, setDisLike] = useState(0)
+
+  const handleCountLike = () => {
+    setLike(like + 1)
+  }
+  const handleCountDislike = () => {
+    setDisLike(dislike + 1)
+  }
   return (
-    <div>Button</div>
+    <div className='container' style={{ display: 'flex', justifyContent: 'center', alignItems: "center", marginTop: '-130px', gap: '15px' }}>
+      <button type='button' onClick={handleCountLike} style={{ color: 'red', backgroundColor: 'black', border: 'none', paddin: '5px', borderRadius: '10px', width: '80px', height: '30px' }}>
+        Likes  {like}
+      </button>
+      <button type='button' onClick={handleCountDislike} style={{ color: 'red', backgroundColor: 'black', border: 'none', paddin: '5px', borderRadius: '10px', width: '80px', height: '30px' }}>
+        Dislikes  {dislike}
+      </button>
+    </div>
   )
 }
 
 export default Button
+
